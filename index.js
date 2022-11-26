93ede2b2767fb4aa1e5eb8d41a24821171043c55
tantaana <shariartanvirofficial@gmail.com>
2022-11-26 14:32:48 +0600

    another one

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,7 +10,48 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+
+
+// -------------------------
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eivtc4s.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('usedProducts').collection('categories');
+        const productsCollection = client.db('usedProducts').collection('products');
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const options = await categoriesCollection.find(query).toArray();
+            res.send(options)
+        })
+
+
+        app.get('/categoryName', async (req, res) => {
+            const query = {};
+            const options = await categoriesCollection.find(query).project({ categories: 1 }).toArray();
+            res.send(options)
+        })
+
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await categoriesCollection.findOne(query);
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
+
+app.get('/', async (req, res) => {
     res.send('valo lagche na ekdom')
 })
 
